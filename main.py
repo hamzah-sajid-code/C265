1247489c811fdad16c35abd56eac0ad752501838
hamzah-sajid-code <82176013+hamzah-sajid-code@users.noreply.github.com>
2023-05-02 16:29:28 +0300

    Init

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+from werkzeug.utils import secure_filename
+from flask import *
+import cv2
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def upload_form():
+    return render_template("upload.html")
+
+@app.route('/', methods=['POST'])
+def upload_video():
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join('static/', filename))
+
+    source = cv2.VideoCapture('static/'+filename)
+
+    frame_width = int(source.get(3))
+    frame_height = int(source.get(4))
+    size = (frame_width, frame_height)
+
+    result = cv2.VideoWriter('static/blackandwhite.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, size, 0)
+
+    try:
+        while True:
+            status, frame_image = source.read()
+            if not status:
+                print("Completed reading all the Frames from the Video")
+                break
+            gray = cv2.cvtColor(frame_image, cv2.COLOR_RGB2GRAY)
+            result.write(gray)
+
+        video_file = 'blackandwhite.mp4'
+
+    except:
+        print("Error occurred while reading video frames.")
+
+    return render_template('upload.html', filename=filename)
+
+@app.route('/download')
+def download_file():
+    video_file = 'blackandwhite.mp4'
+    return send_file('static/'+video_file, as_attachment=True)
+
+@app.route('/display_video/<filename>')
+def display_video(filename):
+    return redirect(url_for('static',filename=filename))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
